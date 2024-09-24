d9416e3808ac94ddaf190a738098f18348c4b330
saurabh110398 <saurabh110398@gmail.com>
2024-09-24 19:38:38 +0530

    5th interview

--- interview-05.js
+++ interview-05.js
@@ -45,7 +45,7 @@
 //      x =10;
 //  }
 //  foo();
-//  console.log( x);//act as aglobal variable
+//  console.log( x);//10 act as aglobal variable
 
 //  function foo(){ 
 //     console.log( x);//error -> not defined 
@@ -87,17 +87,65 @@
 // },0);
 // Promise.resolve().then(()=>console.log('promise'));
 // console.log('end');
+// 
 
-async function foo() {
-   return 'hello'
-   
-    
+// --------------------------------------------------------------------------
+// Promise
+// async function foo() {
+//    return 'hello'
+// }
+// const result = foo();
+// result.then(res=> console.log('res:: ',res))
+
+// console.log('::',result);
+// --------------------------------------------------------------------------
+
+//console.log([1,2] === [1,2]);//false
+
+// --------------------------------------------------------------------------
+
+let user1 ={
+   name:'Pari',
+   age:'15',
+   address: {
+      state:'UP'
+   }
 }
-const result = foo();
-result.then(res=> console.log('res:: ',res)
-)
-console.log('::',result);
+// Shallow copy*************
+// let user2 = user1;
+// let user2 = {...user1}
+// let user2 = Object.assign(user1)
 
+// Deep copy**************
+// let user2 = JSON.parse(JSON.stringify(user1));
+// let user2 = {...user1, address:{...user1.address}}
+// let user2 = structuredClone(user1)//globally available fuction for deep copy
+// user2.name = 'Ramesh';
+// user2.address.state ='MP';
+// console.log('1:: ',user1);
+// console.log('2:: ',user2);
+
+// --------------------------------------------------------------------------
+
+function abc(){
+   
+   // console.log('heyy...');
+   // return 'h111' //----> no effect of return statement on constructor function.
+   // return {
+   //    name:'Ruby'
+   // }//----> return the non primitive values
+   // return [1,2,3]
+   return ()=>{}
+}
+
+const result = new abc();
+console.log(result);;
+
+
+// --------------------------------------------------------------------------
+// --------------------------------------------------------------------------
+// --------------------------------------------------------------------------
+// --------------------------------------------------------------------------
 
 
 
